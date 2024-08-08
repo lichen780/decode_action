5d51b62234645b659c6f9035b6d81d68fc551ea3
action <action@github.com>
2024-08-08 13:13:19 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Wed Aug 07 2024 13:48:32 GMT+0000 (Coordinated Universal Time)
+//Thu Aug 08 2024 13:13:19 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("一键价保");
